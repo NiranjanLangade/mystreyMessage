f076f325e54d84dbd8d06dad5349b9e65cb574d1
Niranjan <niranjan.lanagde21@vit.edu>
2025-03-16 19:23:16 +0530

    navbar and messageCard

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -9,9 +9,9 @@ export async function sendVerificationEmail(
 ): Promise<ApiResponse> {
   try {
     await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: email,
-      subject: 'Mystery Message | Verification Code',
+      from: 'onboarding@resend.dev',
+      to: 'niranjanlangade@gmail.com',
+      subject: 'Mystery Message Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
     return { success: true, message: 'Verification email sent successfully.' };
